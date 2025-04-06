4e7687795a4fc5b7b41e2bcd9f34eda5fde94b2a
wsw0922 <woojoo0922@gmail.com>
2025-04-06 23:39:15 +0900

    chore: define app color palette

---
+++ lib/ui/color_styles.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+abstract class ColorStyles {
+  static const Color black = Color(0xFF252525);
+  static const Color gray1 = Color(0xFFF2F2F2);
+  static const Color gray2 = Color(0xFFDADADC);
+  static const Color gray3 = Color(0xFF6E6E6E);
+  static const Color gray4 = Color(0xFF767676);
+  static const Color gray5 = Color(0xFF8B8C92);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color primary100 = Color(0xFF006DFF);
+  static const Color primary5 = Color(0xFFEBF3FF);
+
+  static const Color labelColorRed100 = Color(0xFF804640);
+  static const Color labelColorRed10 = Color(0xFFFCEEED);
+  static const Color labelColorYellow100 = Color(0xFFAA8339);
+  static const Color labelColorYellow10 = Color(0xFFF4ECDD);
+
+  static const Color warning100 = Color(0xFFC70000);
+  static const Color warning10 = Color(0xFFF3DADA);
+
+  static const Color primaryColor = primary100;
+}
