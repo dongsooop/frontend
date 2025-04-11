52682985d6f9d458e97e0ab7e82c0bfab7685c51
wsw0922 <woojoo0922@gmail.com>
2025-04-11 13:20:05 +0900

    feat: implement initial ChatPage UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dongsoop/presentation/chat/chat_screen.dart';
 import 'package:dongsoop/presentation/home/home_page_screen.dart';
 import 'package:dongsoop/presentation/my_page/my_page_screen.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       routes: {
         // 특정 페이지 확인용
         '/home': (context) => HomePageScreen(),
-        '/mypage': (context) => MyPageScreen()
+        '/mypage': (context) => MyPageScreen(),
+        '/chat': (context) => ChatScreen(),
       },
     );
   }
