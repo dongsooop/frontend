248c931923f71b186f87a4440954f61d65961def
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-04-14 14:39:15 +0900

    chore: 데이터 양식 수정

--- lib/presentation/board/market/temp/temp_market_data.dart
+++ lib/presentation/board/market/temp/temp_market_data.dart
@@ -2,7 +2,7 @@ final List<Map<String, dynamic>> marketList = [
   {
     "market_img": "assets/images/market_test.png",
     "market_title": "DB 프로그래밍 교재",
-    "market_created_at": DateTime(2025, 3, 4, 15, 0),
+    "market_created_at": DateTime.now().subtract(Duration(hours: 5)),
     "market_prices": "10,000",
     "market_messages": "3",
   },
@@ -11,7 +11,7 @@ final List<Map<String, dynamic>> marketList = [
     "market_title": "자바 프로그래밍 교재",
     "market_created_at": DateTime.now().subtract(Duration(hours: 3)), // 3시간 전
     "market_prices": "12,000",
-    "market_messages": "5",
+    "market_messages": 0,
   },
   {
     "market_img": "assets/images/market_test.png",
@@ -19,7 +19,7 @@ final List<Map<String, dynamic>> marketList = [
     "market_created_at":
         DateTime.now().subtract(Duration(minutes: 30)), // 30분 전
     "market_prices": "5,000",
-    "market_messages": "1",
+    "market_messages": 0,
   },
   {
     "market_img": "",
@@ -27,13 +27,13 @@ final List<Map<String, dynamic>> marketList = [
     "market_created_at":
         DateTime.now().subtract(Duration(days: 1, hours: 2)), // 1일 2시간 전
     "market_prices": "8,000",
-    "market_messages": "7",
+    "market_messages": 14,
   },
   {
     "market_img": "assets/images/market_test.png",
     "market_title": "파이썬 문제집",
     "market_created_at": DateTime.now().subtract(Duration(days: 4)), // 4일 전
     "market_prices": "6,000",
-    "market_messages": "0",
+    "market_messages": 0,
   },
 ];
