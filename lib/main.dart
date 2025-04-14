93720d8348fdf54b67f89aa2ab4f223daf0b3442
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-04-15 01:32:53 +0900

    feat: market_write_page UI 구현 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dongsoop/presentation/board/market/list/market_list_page_screen.dart';
+import 'package:dongsoop/presentation/board/market/write/market_write_page_screen.dart';
 import 'package:dongsoop/presentation/board/recruit/list/recruit_list_page_screen.dart';
 import 'package:dongsoop/presentation/board/recruit/write/recruit_write_page_screen.dart';
 import 'package:dongsoop/presentation/chat/chat_screen.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: const [
         Locale('ko', 'KR'), // 한국어
       ],
-      initialRoute: '/home',
+      initialRoute: '/market/write',
       routes: {
         // 특정 페이지 확인용
         '/home': (context) => HomePageScreen(),
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         '/recruit/list': (context) => RecruitListPageScreen(),
         '/recruit/write': (context) => RecruitWritePageScreen(),
         '/market/list': (context) => MarketListPageScreen(),
+        '/market/write': (context) => MarketWritePageScreen(),
       },
     );
   }
