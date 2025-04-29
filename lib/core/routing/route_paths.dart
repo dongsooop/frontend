fad310b3d3901ed9df65d10b5cc38763d003d48e
wsw0922 <woojoo0922@gmail.com>
2025-04-29 14:08:26 +0900

    feat: configure initial routing with tab navigation

---
+++ lib/core/routing/route_paths.dart
@@ -0,0 +1,6 @@
+abstract class RoutePaths {
+  static const String home = '/Home';
+  static const String board = '/Board';
+  static const String chat = '/Chat';
+  static const String mypage = '/Mypage';
+}
