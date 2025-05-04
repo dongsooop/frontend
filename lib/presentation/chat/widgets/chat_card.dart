6874c22724c94cb7cbc38c0869d9d659f17e59f9
wsw0922 <woojoo0922@gmail.com>
2025-05-04 20:13:52 +0900

    refactor: chat screen

--- lib/presentation/chat/widgets/chat_card.dart
+++ lib/presentation/chat/widgets/chat_card.dart
@@ -18,6 +18,91 @@ class ChatCard extends StatefulWidget {
 class _ChatCardState extends State<ChatCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(bottom: 24),
+        height: 48,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 16,
+          children: [
+            ClipOval(
+              child: Image.asset(
+                'assets/images/test.png',
+                width: 48,
+                height: 48,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    spacing: 8,
+                    children: [
+                      Text(
+                        widget.chat.title,
+                        style: TextStyles.normalTextBold.copyWith(
+                            color: ColorStyles.black
+                        ),
+                      ),
+                      Text(
+                        widget.chat.personnel.toString(),
+                        style: TextStyles.smallTextRegular.copyWith(
+                            color: ColorStyles.gray4
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    widget.chat.tag,
+                    style: TextStyles.smallTextRegular.copyWith(
+                        color: ColorStyles.gray4
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              spacing: 8,
+              children: [
+                Text(
+                  widget.chat.time,
+                  style: TextStyles.smallTextRegular.copyWith(
+                      color: ColorStyles.gray4
+                  ),
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 2, horizontal: 6),
+                    decoration: ShapeDecoration(
+                        color: ColorStyles.primaryColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32)
+                        )
+                    ),
+                    child: Text(
+                      widget.chat.messegeNum.toString(),
+                      style: TextStyles.smallTextBold.copyWith(
+                          color: ColorStyles.white
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                )
+              ],
+            ),
+          ],
+        )
+    );
   }
 }
