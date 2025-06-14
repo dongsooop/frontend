e5911fae60d4340747c9084ee6034d0e28a850f5
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-06-15 00:23:28 +0900

    feat: add calendar data layer

--- lib/data/calendar/models/calendar_model.dart
+++ lib/data/calendar/models/calendar_model.dart
@@ -1,43 +1,32 @@
 import 'package:dongsoop/domain/calendar/entities/calendar_entity.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class CalendarModel {
-  final int id;
+part 'calendar_model.freezed.dart';
+part 'calendar_model.g.dart';
+
+@freezed
+@JsonSerializable()
+class CalendarModel with _$CalendarModel {
   final String title;
   final String location;
   final DateTime startAt;
   final DateTime endAt;
-  final String type;
 
   CalendarModel({
-    required this.id,
     required this.title,
     required this.location,
     required this.startAt,
     required this.endAt,
-    required this.type,
   });
 
-  factory CalendarModel.fromJson(Map<String, dynamic> json) {
-    return CalendarModel(
-      id: json['id'] ?? 0,
-      title: json['title'] ?? '',
-      location: json['location'] ?? '',
-      startAt: DateTime.tryParse(json['startAt'] ?? '') ?? DateTime.now(),
-      endAt: DateTime.tryParse(json['endAt'] ?? '') ?? DateTime.now(),
-      type: json['type'] ?? '',
-    );
-  }
-}
+  Map<String, dynamic> toJson() => _$CalendarModelToJson(this);
 
-extension CalendarModelMapper on CalendarModel {
-  CalendarEntity toEntity() {
-    return CalendarEntity(
-      id: id,
-      title: title,
-      location: location,
-      startAt: startAt,
-      endAt: endAt,
-      type: type,
+  factory CalendarModel.fromEntity(CalendarEntity entity) {
+    return CalendarModel(
+      title: entity.title,
+      location: entity.location,
+      startAt: entity.startAt,
+      endAt: entity.endAt,
     );
   }
 }
