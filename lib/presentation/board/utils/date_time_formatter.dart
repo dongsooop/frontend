a5da6b70eb3afae258b29b6dda4e84bc3ea3e278
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-06-28 00:25:05 +0900

    refactor: move datetime formatter

--- lib/presentation/board/utils/date_time_formatter.dart
+++ lib/presentation/board/utils/date_time_formatter.dart
@@ -9,3 +9,8 @@ String formatRelativeTime(DateTime dateTime) {
 
   return '${dateTime.year}. ${dateTime.month}. ${dateTime.day}. ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}';
 }
+
+String formatFullDateTime(DateTime dt) {
+  return '${dt.year}. ${dt.month.toString().padLeft(2, '0')}. ${dt.day.toString().padLeft(2, '0')}. '
+      '${dt.hour.toString().padLeft(2, '0')}:${dt.minute.toString().padLeft(2, '0')}';
+}
