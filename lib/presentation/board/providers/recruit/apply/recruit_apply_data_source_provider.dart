d93efc5b1b6b7ff5364f096c91c9cc0b00b60095
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-07-21 02:35:33 +0900

    refactor: extract data source provider

---
+++ lib/presentation/board/providers/recruit/apply/recruit_apply_data_source_provider.dart
@@ -0,0 +1,11 @@
+import 'package:dongsoop/data/board/recruit/apply/data_sources/recruit_apply_data_source_impl.dart';
+import 'package:dongsoop/providers/auth_dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final recruitApplyDataSourceProvider =
+    Provider<RecruitApplyDataSourceImpl>((ref) {
+  final dio = ref.watch(authDioProvider);
+  final aiDio = createAuthDio(ref: ref, useAi: true);
+
+  return RecruitApplyDataSourceImpl(dio, aiDio);
+});
--- lib/presentation/board/providers/recruit/apply/recruit_decision_use_case_provider.dart
+++ lib/presentation/board/providers/recruit/apply/recruit_decision_use_case_provider.dart
@@ -1,13 +1,10 @@
-import 'package:dongsoop/data/board/recruit/apply/data_sources/recruit_apply_data_source_impl.dart';
 import 'package:dongsoop/data/board/recruit/apply/repositories/recruit_apply_repository_impl.dart';
 import 'package:dongsoop/domain/board/recruit/apply/use_case/recruit_decision_use_case.dart';
-import 'package:dongsoop/providers/auth_dio.dart';
+import 'package:dongsoop/presentation/board/providers/recruit/apply/recruit_apply_data_source_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final recruitDecisionUseCaseProvider = Provider<RecruitDecisionUseCase>((ref) {
-  final dio = ref.watch(authDioProvider);
-
-  final dataSource = RecruitApplyDataSourceImpl(dio);
+  final dataSource = ref.watch(recruitApplyDataSourceProvider);
   final repository = RecruitApplyRepositoryImpl(dataSource);
 
   return RecruitDecisionUseCase(repository);
