46661c5dd6156242f9dd372affa31125b2849aa1
wsw0922 <woojoo0922@gmail.com>
2025-07-23 13:07:17 +0900

    chore: remove unused chat API

--- lib/presentation/board/providers/recruit/recruit_group_chat_use_case_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dongsoop/domain/chat/use_case/create_group_chat_room_use_case.dart';
-import 'package:dongsoop/providers/chat_providers.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final recruitGroupChatUseCaseProvider = Provider<CreateGroupChatRoomUseCase>((ref) {
-  final repository = ref.watch(chatRepositoryProvider);
-  return CreateGroupChatRoomUseCase(repository);
-});
