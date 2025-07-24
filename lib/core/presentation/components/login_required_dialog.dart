193c5c33693988f9d072035ec58c50a596e0778c
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-07-24 13:47:39 +0900

    fix: resolve touch issue on login dialog in real device

--- lib/core/presentation/components/login_required_dialog.dart
+++ lib/core/presentation/components/login_required_dialog.dart
@@ -9,28 +9,35 @@ Future<void> LoginRequiredDialog(BuildContext context) {
   return showGeneralDialog(
     context: context,
     barrierDismissible: false,
-    barrierColor: Colors.transparent,
-    pageBuilder: (_, __, ___) => Stack(
-      children: [
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 1.6, sigmaY: 1.4),
-          child: Container(
-            color: Colors.black.withAlpha((255 * 0.3).round()),
+    barrierColor: Colors.black.withAlpha((255 * 0.3).round()),
+    useRootNavigator: true,
+    pageBuilder: (context, _, __) {
+      return Stack(
+        children: [
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 1.6, sigmaY: 1.4),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+            ),
           ),
-        ),
-        Center(
-          child: CustomConfirmDialog(
-            title: '로그인이 필요해요',
-            content: '이 서비스를 이용하려면\n로그인을 해야 해요!',
-            isSingleAction: false,
-            confirmText: '확인',
-            onConfirm: () {
-              context.push(RoutePaths.signIn);
-            },
-            onCancel: () {},
+          SafeArea(
+            child: Center(
+              child: Material(
+                color: Colors.transparent,
+                child: CustomConfirmDialog(
+                  title: '로그인이 필요해요',
+                  content: '이 서비스를 이용하려면\n로그인을 해야 해요!',
+                  isSingleAction: false,
+                  confirmText: '확인',
+                  onConfirm: () => context.push(RoutePaths.signIn),
+                  onCancel: () {},
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
-    ),
+        ],
+      );
+    },
   );
 }
