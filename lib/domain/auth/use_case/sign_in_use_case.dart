f81fecee6985756006ee8b5e53bc12f27b6e45be
JEON SEUNGBHIN <tmdqls104@naver.com>
2025-08-24 17:09:30 +0900

    chore: rename file and fix imports

--- lib/domain/auth/use_case/sign_in_use_case.dart
+++ lib/domain/auth/use_case/sign_in_use_case.dart
@@ -1,10 +1,10 @@
 import 'package:dongsoop/domain/auth/model/stored_user.dart';
 import 'package:dongsoop/domain/auth/repository/auth_repository.dart';
-import 'package:dongsoop/domain/fcm_token/repository/fcm_token_repository.dart';
+import 'package:dongsoop/domain/device_token/repositoy/device_token_repository.dart';
 
 class SignInUseCase {
   final AuthRepository _authRepository;
-  final FcmTokenRepository _fcmRepository;
+  final DeviceTokenRepository _fcmRepository;
 
   SignInUseCase(this._authRepository, this._fcmRepository);
 
