0059ae33fdd6c9577db796472bbf78fdb312d8ed
wsw0922 <woojoo0922@gmail.com>
2025-09-18 13:55:28 +0900

    feat: empty info text

--- lib/presentation/my_page/activity/blocked_user_screen.dart
+++ lib/presentation/my_page/activity/blocked_user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dongsoop/presentation/my_page/widgets/blocked_user_card.dart';
+import 'package:dongsoop/ui/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -69,7 +70,14 @@ class BlockedUserScreen extends HookConsumerWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-          child: Column(
+          child: list.isEmpty
+          ? Center(
+            child: Text(
+              '차단한 사용자가 없어요',
+              style: TextStyles.normalTextRegular.copyWith(color: ColorStyles.black),
+            ),
+          )
+          : Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
