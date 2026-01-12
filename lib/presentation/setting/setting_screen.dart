19f4131056d2522532d1a473d0c7664a999ba33e
JEON SEUNGBHIN <tmdqls104@naver.com>
2026-01-12 22:42:25 +0900

    chore: update to v.1.6.0

--- lib/presentation/setting/setting_screen.dart
+++ lib/presentation/setting/setting_screen.dart
@@ -63,7 +63,7 @@ class SettingScreen extends HookConsumerWidget {
               title: '이용 안내',
               children: [
                 buildSettingsItem(
-                  label: '버전  1.5.10',
+                  label: '버전  1.6.0',
                   onTap: () {},
                 ),
                 buildSettingsItem(
