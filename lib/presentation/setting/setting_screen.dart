8d8dce9ab388539cd704a954749d41cce9ba68bb
wsw0922 <woojoo0922@gmail.com>
2026-01-16 21:00:09 +0900

    chore: update to v.1.7.1+33

--- lib/presentation/setting/setting_screen.dart
+++ lib/presentation/setting/setting_screen.dart
@@ -68,7 +68,7 @@ class SettingScreen extends HookConsumerWidget {
               title: '이용 안내',
               children: [
                 buildSettingsItem(
-                  label: '버전  1.7.0',
+                  label: '버전  1.7.1',
                   onTap: () {},
                 ),
                 buildSettingsItem(
