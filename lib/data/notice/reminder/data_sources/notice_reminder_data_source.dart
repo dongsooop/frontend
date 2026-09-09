96063838d3fee98d5fa0df150488096af5ac9c21
rdyjun <rdyjun00@gmail.com>
2026-09-09 06:35:39 -0700

    feat: add notice_reminder_data_source

---
+++ lib/data/notice/reminder/data_sources/notice_reminder_data_source.dart
@@ -0,0 +1,10 @@
+abstract class NoticeReminderDataSource {
+  Future<void> upsert({
+    required int noticeId,
+    required DateTime remindAt,
+  });
+
+  Future<void> delete({
+    required int noticeId,
+  });
+}
