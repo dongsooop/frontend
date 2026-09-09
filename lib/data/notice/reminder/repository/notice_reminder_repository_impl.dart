884831faf572a1320ba96e4433b66e07ad93016b
rdyjun <rdyjun00@gmail.com>
2026-09-09 07:13:47 -0700

    refactor: align reminder repository error handling

--- lib/data/notice/reminder/repository/notice_reminder_repository_impl.dart
+++ lib/data/notice/reminder/repository/notice_reminder_repository_impl.dart
@@ -1,26 +1,48 @@
+import 'package:dongsoop/core/exception/exception.dart';
+import 'package:dongsoop/core/network/error_handler_mixin.dart';
 import 'package:dongsoop/data/notice/reminder/data_sources/notice_reminder_data_source.dart';
 import 'package:dongsoop/domain/notice/reminder/repository/notice_reminder_repository.dart';
 
-class NoticeReminderRepositoryImpl implements NoticeReminderRepository {
+class NoticeReminderRepositoryImpl
+    with ErrorHandlerMixin
+    implements NoticeReminderRepository {
   final NoticeReminderDataSource _dataSource;
 
   NoticeReminderRepositoryImpl(this._dataSource);
 
   @override
-  Future<void> upsert({
+  Future<void> setReminder({
     required int noticeId,
     required DateTime remindAt,
   }) {
-    return _dataSource.upsert(
-      noticeId: noticeId,
-      remindAt: remindAt,
+    return _handle(
+      () => _dataSource.setReminder(
+        noticeId: noticeId,
+        remindAt: remindAt,
+      ),
     );
   }
 
   @override
-  Future<void> delete({
+  Future<void> deleteReminder({
     required int noticeId,
   }) {
-    return _dataSource.delete(noticeId: noticeId);
+    return _handle(
+      () => _dataSource.deleteReminder(noticeId: noticeId),
+    );
+  }
+
+  Future<T> _handle<T>(Future<T> Function() action) async {
+    try {
+      return await action();
+    } on SessionExpiredException {
+      rethrow;
+    } catch (e, st) {
+      final converted = convertError(e);
+      if (converted is SessionExpiredException) {
+        throw converted;
+      }
+      Error.throwWithStackTrace(NoticeReminderException(), st);
+    }
   }
 }
