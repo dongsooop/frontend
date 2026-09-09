6b88b2dc744721cffd2d5f39a87669d115e82172
rdyjun <rdyjun00@gmail.com>
2026-09-09 07:14:36 -0700

    refactor: align reminder data source response handling

--- lib/data/notice/reminder/data_sources/notice_reminder_data_source_impl.dart
+++ lib/data/notice/reminder/data_sources/notice_reminder_data_source_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dongsoop/core/http_status_code.dart';
 import 'package:dongsoop/data/notice/reminder/data_sources/notice_reminder_data_source.dart';
 
 class NoticeReminderDataSourceImpl implements NoticeReminderDataSource {
@@ -11,18 +12,26 @@ class NoticeReminderDataSourceImpl implements NoticeReminderDataSource {
     required int noticeId,
     required DateTime remindAt,
   }) async {
-    await _authDio.put(
+    final response = await _authDio.put(
       '/notice/$noticeId/reminder',
       data: {
         'remindAt': remindAt.toIso8601String(),
       },
     );
+
+    if (response.statusCode != HttpStatusCode.ok.code) {
+      throw Exception('status: ${response.statusCode}');
+    }
   }
 
   @override
   Future<void> deleteReminder({
     required int noticeId,
   }) async {
-    await _authDio.delete('/notice/$noticeId/reminder');
+    final response = await _authDio.delete('/notice/$noticeId/reminder');
+
+    if (response.statusCode != HttpStatusCode.noContent.code) {
+      throw Exception('status: ${response.statusCode}');
+    }
   }
 }
