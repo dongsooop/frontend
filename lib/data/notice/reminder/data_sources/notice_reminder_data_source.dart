6c933234956000676180b469ab4e47dab446d1db
rdyjun <rdyjun00@gmail.com>
2026-09-09 07:13:30 -0700

    refactor: align reminder data source method names

--- lib/data/notice/reminder/data_sources/notice_reminder_data_source.dart
+++ lib/data/notice/reminder/data_sources/notice_reminder_data_source.dart
@@ -1,10 +1,10 @@
 abstract class NoticeReminderDataSource {
-  Future<void> upsert({
+  Future<void> setReminder({
     required int noticeId,
     required DateTime remindAt,
   });
 
-  Future<void> delete({
+  Future<void> deleteReminder({
     required int noticeId,
   });
 }
