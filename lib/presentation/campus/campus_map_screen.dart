2101faa697f979aa809994fef50a5e160e5ad107
rdyjun <rdyjun00@gmail.com>
2026-09-10 20:07:33 -0700

    feat: add campus map screen placeholder

---
+++ lib/presentation/campus/campus_map_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CampusMapScreen extends StatelessWidget {
+  const CampusMapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Campus map placeholder')),
+    );
+  }
+}
